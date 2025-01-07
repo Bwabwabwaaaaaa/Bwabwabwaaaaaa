e35bd83e929851cfdf02cdf4f9da3b280acb3dd7
Bwabwabwaaaaaa <mahaultreynaud@icloud.com>
2025-01-07 11:51:03 +0100

    Update persoscript.js

--- projet2/scripts/persoscript.js
+++ projet2/scripts/persoscript.js
@@ -11,27 +11,26 @@ function valider() {
 	var perso_choisi = perso.options[perso.selectedIndex].value;
 	
 	// Ensuite on ajoute la valeur dans la variable correspondante 
-	// Par exemple si l'utilisateur a choisi expelliarmus alors on ajoute 1 à la variable griffondor
 	if(perso_choisi=="p")
 	{
 		psycopathe=psycopathe+1;
 	}
-	else if(perso_choisi=="s")
+	else if (perso_choisi=="s")
 	{
 		sociopathe=sociopathe+1;
 	}
-	else
+	else 
 	{
 		nevropathe=nevropathe+1;
 	}
 	
 	//On regroupe les différents profils dans un tableau 
-	var valeurs=[psycopathe,sociopathe,nevropathe,rien];
-	var profils=["Psycopathe, tu es quelqu'un qu'il faut tenir à l'écart de la sociétée","Sociopathe, tu est quelqu'un qu'il faut tenir à l'écart de la sociétée","Nevropathe, tu es triste, tu le sais et tu le fais savoir au monde","vraiment indécis, tu sais qu'il faut choisir ?"];
+	var valeurs=[psycopathe,sociopathe,nevropathe];
+	var profils=["Psycopathe, tu es quelqu'un qu'il faut tenir à l'écart de la sociétée","Sociopathe, tu est quelqu'un qu'il faut tenir à l'écart de la sociétée","Nevropathe, tu es triste, tu le sais et tu le fais savoir au monde"];
 	
 	//On cherche le maximum du tableau et son indice
 	var max = valeurs[0];
-    var indice_du_max = 0;
+    var indindice_du_max = 0;
     
     for (var i = 1; i < valeurs.length; i++) {
         if (valeurs[i] > max) {
